f14e42e631edb90e7d470d8ae888674b6fba6f46
Craig Cockrum <craigcockrum@MacBook-Pro.local>
2020-06-17 14:59:53 -0500

    test

--- project_1/app.js
+++ project_1/app.js
@@ -6,7 +6,7 @@ const testURL = "https://gateway.marvel.com:443/v1/public/characters?apikey=ff41
 
 const baseURL = "https://gateway.marvel.com:443/v1/public/characters?"
 const pubKey = "apikey=ff411374090a15671c59a2fe9af6fe8c"
-const ts = "ts=1"
+const ts = "ts=1592423954"
 const hash = "hash=5382342d81bb0e9ef84742f7d29632fa"
 let queryURL = baseURL + ts + '&' + pubKey + '&' + hash
 console.log(testURL);
