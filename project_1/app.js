23d7d67d400ab93e05186e23c313566c94ab4d02
Craig Cockrum <craigcockrum@MacBook-Pro.local>
2020-06-17 18:57:02 -0500

    2 character search working

--- project_1/app.js
+++ project_1/app.js
@@ -8,7 +8,7 @@ const pubKey = "apikey=ff411374090a15671c59a2fe9af6fe8c"
 const ts = "ts=1592423954"
 const hash = "hash=cffe958a285936c219aa88249b47aab1"
 const queryType = "name="
-let nameQuery = "Spider-Man"
+let nameQuery = ""
 let queryURL = baseURL + queryType 
 
 $(()=> {
@@ -20,12 +20,15 @@ const getCharacter = () => {
         <div id="name"> ${characterData.data.results[0].name}</div>
         <div id="bio"> ${characterData.data.results[0].description}</div>
         `)
+        const $img = $('<img id="thumbnail>').attr('src',characterData.data.results[0].thumbnail.path+'.jpg')
+        $('#character-container').prepend($img)
         console.log(characterData.data.results[0].name)
+        console.log($img)
     }, (error) =>{
         console.log('error')
     })
 }
-
+// Get Name of character from input box
 $('form').on('submit', (event) => {
     event.preventDefault()
     nameQuery = $('input[type=text]').val()
