562057a92927715e53fac81fa08d52575ee13376
Craig Cockrum <craigcockrum@MacBook-Pro.local>
2020-06-17 18:30:50 -0500

    basic functionality with one character working

--- project_1/app.js
+++ project_1/app.js
@@ -1,13 +1,35 @@
 console.log('App is connected');
 
-const testURL = "https://gateway.marvel.com:443/v1/public/characters?apikey=ff411374090a15671c59a2fe9af6fe8c"
 
 
 
 const baseURL = "https://gateway.marvel.com:443/v1/public/characters?"
 const pubKey = "apikey=ff411374090a15671c59a2fe9af6fe8c"
 const ts = "ts=1592423954"
-const hash = "hash=5382342d81bb0e9ef84742f7d29632fa"
-let queryURL = baseURL + ts + '&' + pubKey + '&' + hash
-console.log(testURL);
-console.log(queryURL);
+const hash = "hash=cffe958a285936c219aa88249b47aab1"
+const queryType = "name="
+let nameQuery = "Spider-Man"
+let queryURL = baseURL + queryType 
+
+$(()=> {
+const getCharacter = () => {
+    $.ajax({
+        url: queryURL + nameQuery + '&' + ts + '&' + pubKey + '&' + hash
+    }).then((characterData) => {
+        $('#character-container').html(`
+        <div id="name"> ${characterData.data.results[0].name}</div>
+        <div id="bio"> ${characterData.data.results[0].description}</div>
+        `)
+        console.log(characterData.data.results[0].name)
+    }, (error) =>{
+        console.log('error')
+    })
+}
+
+$('form').on('submit', (event) => {
+    event.preventDefault()
+    nameQuery = $('input[type=text]').val()
+    console.log(nameQuery)
+    getCharacter()
+})
+})
