aa187ae731f9eb3211206838c0925a7cc750e196
Craig Cockrum <craigcockrum@MacBook-Pro.local>
2020-06-17 12:48:12 -0500

    testing api

--- project_1/app.js
+++ project_1/app.js
@@ -1,1 +1,13 @@
 console.log('App is connected')
+
+const baseURl = "https://gateway.marvel.com:443/v1/public/characters?";
+console.log(baseURl)
+
+
+const key = "apikey=ff411374090a15671c59a2fe9af6fe8c"
+let timestamp ="ts=1"
+const hash = "hash=982b87adee39429590ab92ee9d7e0946"
+let queryURL = baseURl + timestamp + "&" + key + "&" + hash
+
+console.log(queryURL)
+
