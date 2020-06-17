c3c4f466b004964de22138c71f5dc4d4a1b4d627
Craig Cockrum <craigcockrum@MacBook-Pro.local>
2020-06-17 13:13:28 -0500

    starting over

--- project_1/app.js
+++ project_1/app.js
@@ -1,15 +1,5 @@
-console.log('App is connected')
-
-const baseURL = "https://gateway.marvel.com:443/v1/public/characters?apikey=ff411374090a15671c59a2fe9af6fe8c";
-const key = "apikey=ff411374090a15671c59a2fe9af6fe8c"
-let testUrl = baseURL + key
+console.log('App is connected');
 
 
 
-
-// let timestamp ="ts=1"
-const hash = "hash=982b87adee39429590ab92ee9d7e0946"
-let queryURL = baseURL + timestamp + "&" + key + "&" + hash
-console.log(baseURL)
-console.log(queryURL)
 
