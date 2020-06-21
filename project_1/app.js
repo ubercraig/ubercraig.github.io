c49af7c2e694a0e21ad488e6e103f526a37f72fd
Craig Cockrum <craigcockrum@MacBook-Pro.local>
2020-06-21 16:29:15 -0500

    about page created and linked

--- project_1/app.js
+++ project_1/app.js
@@ -9,13 +9,11 @@ const ts = "ts=1592423954"
 const hash = "hash=cffe958a285936c219aa88249b47aab1"
 const queryType = "name="
 let nameQuery = ""
-let seriesQuery = ""
 let queryURL = baseURL + queryType 
 
 
 $(()=> {
 const getCharacter = () => {
- 
     // ajax functionality modified from class example
     $.ajax({
         url: queryURL + nameQuery + '&' + ts + '&' + pubKey + '&' + hash
@@ -26,14 +24,15 @@ const getCharacter = () => {
         `)
         const $img = $('<img>').attr('src',characterData.data.results[0].thumbnail.path+'.jpg')
         $('#character-container').prepend($img)
-        // console.log(characterData.data.results[0].name)  /// for debug
        
         //series button functionality
         $('.series').on('click', (event) => {
             event.preventDefault()
             $('.series-results').remove()
+            console.log(characterData.data.results[0].name)  /// for debug
             for (i=0; i <=20; i++) {
             let series = $('<div class="series-results">').text(characterData.data.results[0].series.items[i].name)
+            console.log(characterData.data.results[0].series.items[i].name)
             $('#series-container').append(series)
             }
         })
@@ -43,7 +42,7 @@ const getCharacter = () => {
             event.preventDefault()
             $('.events-results').remove()
             for (i=0; i <=20; i++) {
-            let events = $('<div class="events-results">').text(characterData.data.results[0].events.items[i].name)
+            events = $('<div class="events-results">').text(characterData.data.results[0].events.items[i].name)
             $('#events-container').append(events)
             }
         })
@@ -56,6 +55,7 @@ $('form').on('submit', (event) => {
     $('img').remove()
     $('.events-results').remove()
     $('.series-results').remove()
+    
     event.preventDefault()
     nameQuery = $('input[type=text]').val()
     console.log(nameQuery)
